1cfedfc2d3acf91759acbf4321d0bf3df6b2a44f
RyanSuda-Code <sudaryan49@yahoo.com>
2026-02-22 15:03:52 +0900

    Progress: Updated the custom header javasccript to search the custom custome element instead of the whole document.

--- starter-code/JavaScript/script.js
+++ starter-code/JavaScript/script.js
@@ -1,6 +1,6 @@
-class SiteHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
+class SiteHeader extends HTMLElement {  /*i created a new class called SiteHeader using the syntax class, that would behave like a normal HTML element.*/
+    connectedCallback() { /*runs automatically when my custom element is added to the webpage. so it runs when <site-header></site-header> appears inside the html.*/
+        this.innerHTML = ` 
             <header>
                 <img src="./assets/logo.svg" alt="myteam logo" class="logo">
                 
@@ -36,9 +36,9 @@ class SiteHeader extends HTMLElement {
             </header>
         `;
 
-        const openBtn = document.querySelector(".hamburger-btn");
-        const closeBtn = document.querySelector(".close-btn");
-        const mobileNav = document.querySelector(".mobile-nav");
+        const openBtn = this.querySelector(".hamburger-btn"); /*by using this. syntax it will only search for this classes inside this custom element. */
+        const closeBtn = this.querySelector(".close-btn"); /*If i used the document. syntax it may cause an incorrect behavior, because it will search the whole html for the class and if i have multiple classes with thte same name it will only affect the first one it will find and igonore the rest. */
+        const mobileNav = this.querySelector(".mobile-nav");
 
         openBtn.addEventListener("click", () => {
             mobileNav.classList.add("active");
@@ -51,7 +51,7 @@ class SiteHeader extends HTMLElement {
     }
 }
 
-customElements.define("site-header", SiteHeader);
+customElements.define("site-header", SiteHeader); /*customElements is where you can store your new custom elements. With define("site-header", SiteHeader) i register the custom element site-header with the class SiteHeader.  */
 
 
 class SiteFooter extends HTMLElement {
